1250b7ead1d806edcd34aa10527245008affb2b5
KurenPeng <>
2021-05-09 12:00:19 +0800

    add handleDistribution function

--- src/component/Room.jsx
+++ src/component/Room.jsx
@@ -1,36 +1,42 @@
 import React, { useCallback, useEffect, useState } from 'react';
 
-const Room = ({ room }) => {
-    const [adultCount, setAdultCount] = useState(0);
-    const [childrenCount, setChildrenCount] = useState(0);
-    const [totalCount, setTotalCount] = useState(0);
+const Room = ({ room, index, handleDistribution }) => {
+    const [adultCount, setAdultCount] = useState('0');
+    const [childrenCount, setChildrenCount] = useState('0');
+    // const [totalCount, setTotalCount] = useState(0);
 
     const handleChangeCount = (e) => {
         if (e.target.name === "adult") {
-            setAdultCount(e.target.value)
+            setAdultCount(e.target.value);
         }
         if (e.target.name === "children") {
-            setChildrenCount(e.target.value)
+            setChildrenCount(e.target.value);
         }
+        handleDistribution(index, adultCount, childrenCount)
     }
-
     useEffect(() => {
-        setTotalCount(Number(adultCount) + Number(childrenCount))
-    }, [Number(adultCount), Number(childrenCount)])
+        handleDistribution(index, adultCount, childrenCount)
+    }, [adultCount, childrenCount])
+    // useEffect(() => {
+    //     setTotalCount(Number(adultCount) + Number(childrenCount))
+    // }, [Number(adultCount), Number(childrenCount)])
 
-    console.log(totalCount);
+    // console.log(totalCount);
 
     return (
         <div className="people-statistics-wrapper">
+            <div className="room-info">
+                房間：2 人
+                    </div>
             <div className="adult">
                 <div className="adult-left">
                     <div>大人</div>
                     <div>年齡 20+</div>
                 </div>
                 <div className="adult-right">
-                    <div>+</div>
-                    <input name="adult" min={room.min} max={room.max - childrenCount} type="number" value={adultCount} onChange={handleChangeCount} />
                     <div>-</div>
+                    <input name="adult" min={room.min} max={room.max - childrenCount} type="number" value={adultCount} onChange={handleChangeCount} />
+                    <div>+</div>
                 </div>
             </div>
             <div className="adult">
@@ -39,9 +45,9 @@ const Room = ({ room }) => {
                     <div>年齡 0+</div>
                 </div>
                 <div className="adult-right">
-                    <div>+</div>
-                    <input name="children" min={room.min} max={room.max - adultCount} type="number" value={childrenCount} onChange={handleChangeCount} />
                     <div>-</div>
+                    <input name="children" min={room.min} max={room.max - adultCount} type="number" value={childrenCount} onChange={handleChangeCount} />
+                    <div>+</div>
                 </div>
             </div>
         </div>
--- src/index.jsx
+++ src/index.jsx
@@ -5,10 +5,30 @@ import './styles/global.scss'
 const Index = () => {
     const [people, setPeople] = useState(0);
     const [rooms, setRooms] = useState([]);
+    const [distribution, setDistribution] = useState([{ adult: 0, child: 0 }, { adult: 0, child: 0 }]);
 
-    const handleDistribution = () => {
+    const handleDistribution = (index, adultCount, childrenCount) => {
+        distribution[index] = {
+            ...distribution[index],
+            adult: Number(adultCount),
+            child: Number(childrenCount)
+        }
+        setDistribution([
+            ...distribution,
+        ])
 
     }
+    console.log(distribution);
+
+    const adultTotal = distribution.reduce((accumulator, currentValue) => {
+        return accumulator + currentValue.adult;  // 與前一個值相加
+    }, 0);
+
+    const childTotal = distribution.reduce((accumulator, currentValue) => {
+        return accumulator + currentValue.child;  // 與前一個值相加
+    }, 0);
+
+
     useEffect(() => {
         setRooms([...rooms, {
             min: 0,
@@ -20,17 +40,18 @@ const Index = () => {
         ])
     }, []);
 
+    useEffect(() => {
+        setPeople(adultTotal + childTotal)
+    }, [adultTotal, childTotal])
+
     return (
         <div className='container'>
             <header>oyaisa</header>
             <div className="app-wrapper">
-                <div className="booking-info">住客人數：3 人 / 2 房</div>
+                <div className="booking-info">{`住客人數：${people} 人 / ${rooms.length} 房`}</div>
                 <div className="room-wrapper">
-                    <div className="room-info">
-                        房間：2 人
-                    </div>
                     {rooms.map((room, index) =>
-                        <Room key={`room_${index}`} room={room}></Room>
+                        <Room key={`room_${index}`} room={room} index={index} distribution={distribution} handleDistribution={handleDistribution}></Room>
                     )}
                 </div>
             </div>
